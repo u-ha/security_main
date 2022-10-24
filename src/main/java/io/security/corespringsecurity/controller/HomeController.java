a96cb32dfa7cf5a29b2361b44c857d7d6e7334a4
uha49 <uha49432110@gmail.com>
2022-10-24 16:49:44 +0900

    security-project(2)

--- src/main/java/io/security/corespringsecurity/controller/HomeController.java
+++ src/main/java/io/security/corespringsecurity/controller/HomeController.java
@@ -3,12 +3,18 @@ package io.security.corespringsecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping(value="/")
 	public String home() throws Exception {
+		return "home";
+	}
+
+	@PostMapping(value="/")
+	public String home2() throws Exception {
 		return "home";
 	}
 
